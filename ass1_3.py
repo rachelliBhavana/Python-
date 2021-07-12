69f169663c943d8790c2f3025612e2a5dfa5e4d0
rachelliBhavana <86688714+rachelliBhavana@users.noreply.github.com>
2021-07-12 21:10:34 +0530

    Write a program which display 10 to 1 on screen.

---
+++ ass1_3.py
@@ -0,0 +1,20 @@
+'''
+Write a program which display 10 to 1 on screen.  
+Output : 10 9 8 7 6 5 4 3 2 1  
+'''
+'''
+Function Name : xyz
+Description :  display 10 to 1 on screen.
+Output : 10 9 8 7 6 5 4 3 2 1  
+Author : Bhavana Rachelli
+Date : 12 July 2021
+'''
+def xyz():
+ for i in range(10,0,-1):
+  print(i,end=" ")
+ 
+def main():
+ xyz()
+ 
+if __name__=="__main__":
+ main() 
