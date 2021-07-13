304526e1e4c84f476a87701fb8d3a2d5cc671ae5
rachelliBhavana <86688714+rachelliBhavana@users.noreply.github.com>
2021-07-13 19:40:00 +0530

    Write a program which accept name from user and display length of its name.  Input : Marvellous Output : 10

---
+++ ass1_6.py
@@ -0,0 +1,10 @@
+'''
+Write a program which accept name from user and display length of its name. 
+Input : Marvellous Output : 10 
+'''
+def main():
+  Str_value=(input("enter your name:"))
+  print("length of given name is", len(Str_value))
+
+if __name__=="__main__":
+ main() 
