ab5ae00dda9d19c8b1627a1456af4c171f9cdec9
rachelliBhavana <86688714+rachelliBhavana@users.noreply.github.com>
2021-07-29 23:04:46 +0530

    Add files via upload

---
+++ dict2.py
@@ -0,0 +1,7 @@
+def main():
+  batches = {"PPA":12500, "LB":11000, "Python":13000, "Angular" : 10000}
+  print("enter the batch name")
+  name=input()
+  print(batches.get(name,"there is no such batch"))
+if __name__=="__main__":
+ main()  
