ca9099059b3dd1af9538b4550bccf325208cb39a
rachelliBhavana <86688714+rachelliBhavana@users.noreply.github.com>
2021-08-06 19:33:00 +0530

    Add files via upload

---
+++ seq.py
@@ -0,0 +1,10 @@
+def main():
+    arr=(10,20,30)
+    print("type of array is",type(arr))
+    brr=(10)
+    print("type of brr is :",type(brr))
+    crr=(10,)
+    print("type of brr is :",type(arr))
+    
+if __name__=="__main__":
+  main()
